13dcc68fd7e487bb412895f0c1a628d4c3e6b7ea
I514692 <klose.wu@sap.com>
2019-09-13 16:55:17 +0800

    use SPort in main method

--- src/main/java/org/klose/scheme/Application.java
+++ src/main/java/org/klose/scheme/Application.java
@@ -1,7 +1,7 @@
 package org.klose.scheme;
 
-
 import org.apache.commons.lang3.StringUtils;
+import org.klose.scheme.io.SPort;
 import org.klose.scheme.model.SExpression;
 import org.klose.scheme.model.SFrame;
 import org.klose.scheme.service.EvalService;
@@ -20,69 +20,69 @@ import java.util.concurrent.Future;
  */
 public class Application {
 
-    public static void main(String[] args) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        SFrame rootEnv = InitEnv.init();
+  public static void main(String[] args) {
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+    SFrame rootEnv = InitEnv.init();
 
-        //repl: loop of read -> eval -> print -> read
-        final Runnable repl = () -> {
-            String expression = "";
-            String line;
-            boolean newEval = true;
-            while (true) {
-                if (newEval)
-                    System.out.print(">> ");
+    //repl: loop of read -> eval -> print -> read
+    final Runnable repl = () -> {
+      String expression = "";
+      String line;
+      boolean newEval = true;
+      while (true) {
+        if (newEval)
+          System.out.print(">> ");
 
-                try {
-                    line = console.readLine();
-                    if (line == null)
-                        System.exit(0);
+        try {
+          line = console.readLine();
+          if (line == null)
+            System.exit(0);
 
-                    expression = expression + " " + line + " ";
-                    if (inputNotFinished(expression)) {
-                        newEval = false;
-                        continue;
-                    }
+          expression = expression + " " + line + " ";
+          if (inputNotFinished(expression)) {
+            newEval = false;
+            continue;
+          }
 
-                    if (!StringUtils.isEmpty(expression.trim())) {
-                        SExpression exp = SParser.parse(expression);
-                        SObject val = EvalService.eval(exp, rootEnv);
-                        System.out.println(val);
-                    } else {
-                        System.err.println("empty input !");
-                    }
-                } catch (Throwable e) {
-                    System.err.println(e.getMessage());
-                }
-                newEval = true;
-                expression = "";
-            }
-        };
-        final Future<?> future = executor.submit(repl);
-
-        // gracefully shutdown jvm
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            //shutdown repl thread
-            future.cancel(true);
-            System.out.println();
-            System.out.println();
-            System.out.println("Moriturus te saluto.");
-        }));
-    }
-
-    private static boolean inputNotFinished(String str) {
-        int startCharCount = 0, endCharCount = 0;
-        for (char c : str.toCharArray()) {
-            if (c == '(')
-                startCharCount++;
-            if (c == ')')
-                endCharCount++;
+          if (!StringUtils.isEmpty(expression.trim())) {
+            SExpression exp = SParser.parse(new SPort(expression));
+            SObject val = EvalService.eval(exp, rootEnv);
+            System.out.println(val);
+          } else {
+            System.err.println("empty input !");
+          }
+        } catch (Throwable e) {
+          System.err.println(e.getMessage());
         }
-        return startCharCount > endCharCount;
-    }
+        newEval = true;
+        expression = "";
+      }
+    };
+    final Future<?> future = executor.submit(repl);
 
-    private Application() {
-        throw new UnsupportedOperationException("illegal constructor for Application class");
+    // gracefully shutdown jvm
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      //shutdown repl thread
+      future.cancel(true);
+      System.out.println();
+      System.out.println();
+      System.out.println("Moriturus te saluto.");
+    }));
+  }
+
+  private static boolean inputNotFinished(String str) {
+    int startCharCount = 0, endCharCount = 0;
+    for (char c : str.toCharArray()) {
+      if (c == '(')
+        startCharCount++;
+      if (c == ')')
+        endCharCount++;
     }
+    return startCharCount > endCharCount;
+  }
+
+  private Application() {
+    throw new UnsupportedOperationException("illegal constructor for Application class");
+  }
 }
